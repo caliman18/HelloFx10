5c33751e0c58b8f530b43c04e817ebae4bc982e0
David <caliman18@hotmail.com>
2018-09-06 15:30:03 -0400

    created user login application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,24 @@
 package sample;
 
+//import java.awt.Label;
+//import java.awt.TextField;
 import javafx.application.Application;
 
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 
 
 import javafx.event.ActionEvent;
@@ -17,24 +31,56 @@ import javafx.scene.layout.StackPane;
 public class Main extends Application {public static void main(String[] args) {
     launch(args);
 }
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("JavaFX Welcome");
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    GridPane grid = new GridPane();
+    grid.setAlignment(Pos.CENTER);
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(25, 25, 25, 25));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+    Text scenetitle = new Text("Welcome");
+    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+    grid.add(scenetitle, 0, 0, 2, 1);
+
+    Label userName = new Label("User Name:");
+    grid.add(userName, 0, 1);
+
+    TextField userTextField = new TextField();
+    grid.add(userTextField, 1, 1);
+
+    Label pw = new Label("Password:");
+    grid.add(pw, 0, 2);
+
+    PasswordField pwBox = new PasswordField();
+    grid.add(pwBox, 1, 2);
+
+    Button btn = new Button("Sign in");
+    HBox hbBtn = new HBox(10);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    hbBtn.getChildren().add(btn);
+    grid.add(hbBtn, 1, 4);
+
+    final Text actiontarget = new Text();
+    grid.add(actiontarget, 1, 6);
+
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+      @Override
+      public void handle(ActionEvent e) {
+        actiontarget.setFill(Color.FIREBRICK);
+        actiontarget.setText("Sign in button pressed");
+      }
+    });
+
+    Scene scene = new Scene(grid, 300, 275);
+    primaryStage.setScene(scene);
+
+    primaryStage.show();
+  }
+
 }
 
